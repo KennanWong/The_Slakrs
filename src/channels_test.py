0f7d17ef2a1231b98e9094a2a0d50c1fcc8670fa
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-08 22:13:19 +1100

    made new changes to the functions

--- src/channels_test.py
+++ src/channels_test.py
@@ -1,7 +1,7 @@
 import pytest
 from auth import auth_register
 from channel import channel_details
-from channels import channels_create
+from channels import channels_create, channels_list, channels_listall
 from error import InputError, AccessError
 
 
@@ -15,20 +15,20 @@ def test_list_and_details():
     token1 = results['token']
     u_id1 = results['u_id']
 
+    #returns channel_id
     channel_info1 = channels_create(token1, 'Slakrs', False)
 
     channel = channel_details(token1, channel_info1)
 
-    channel_member = channels_list(token1)
-    
-    flag = 0
-    j = 0
+    #returns channels user with associated token is part of 
 
-    for j in channel_member:
-        if channel_info1 == channel_member[j]:
-            flag = 1
-        j =+ 1
+    member = channels_list(token1)
+    channel_member = member['channel_id']
+    flag = 0
     
+    if any(c[channel_info1] for c in channel_member):
+        flag = 1
+
     assert flag == 1
 
 '''
@@ -45,20 +45,19 @@ def test_listall_and_details():
     token2 = results['token']
     u_id2 = results['u_id']
 
-    channel_info1 = channels_create(token1, 'Slakrs', False)
+    channel_info1 = channels_create(token1, 'Slakrs', True)
     channel_info2 = channels_create(token2, 'Kings Landing' , True)
 
     channel1 = channel_details(token1, channel_info1)
     channel2 = channel_details(token2, channel_info2)
 
-    channel_member1 = channels_listall(token1)
-    
+    member1 = channels_listall(token2)
     
     flag = 0
     j = 0
 
-    for j in channel_member:
-        if (channel_info1 or channel_info2) == channel_member1[j]:
+    for i in member1:
+        if (channel_info1 or channel_info2) == member1[j]:
             flag = 1
         j =+ 1
     

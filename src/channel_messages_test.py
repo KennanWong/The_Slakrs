5cde4cf7e9e256cd8d5ce1d509e8ec5df45ecf23
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-08 14:46:12 +1100

    Added messages_send but not confirmed yet

--- src/channel_messages_test.py
+++ src/channel_messages_test.py
@@ -3,6 +3,7 @@ from error import InputError, AccessError
 from auth import auth_register
 from channel import channel_messages
 from channels import channels_create
+from message import message_send
 
 '''
 #############################################################
@@ -18,17 +19,20 @@ AccessError when
 '''
 
 def test_channel_messages_clean():
+	# CASE 1: No messages
     user1 = auth_register("user1@gmail.com", '123!@asdf', 'user1', 'Smith')
     token1 = user1['token']
 
     channelInfo = channels_create(token1, 'The Slakrs', True)
     channel_id = channelInfo['channel_id']
 
+    # No message sent to channel
     assert channel_messages(token1, channel_id, 0)["messages"] == []
     assert channel_messages(token1, channel_id, 0)["start"] == 0
     assert channel_messages(token1, channel_id, 0)["end"] == -1
 
 def test_channel_messages_invalid_channel():
+	# CASE 2: Messages in an invalid channel
     user1 = auth_register("user1@gmail.com", '123!@asdf', 'user1', 'Smith')
     token1 = user1['token']
     u_id1 = user1['u_id']
@@ -37,20 +41,30 @@ def test_channel_messages_invalid_channel():
     channel_id = channelInfo['channel_id']
     invalidChannelID = 1
 
+    # Send message
+    message_send(token1, channel_id, "hello")
+
+	# InputError when we check messages in an invalid channel
     with pytest.raises(InputError) as e:
         channel_messages(token1, invalidChannelID, 0)
 
-def test_channel_messages_start_plus():
+def test_channel_messages_start_excess():
+	# CASE 3: When start > total messages
     user1 = auth_register("user1@gmail.com", '123!@asdf', 'user1', 'Smith')
     token1 = user1['token']
 
     channelInfo = channels_create(token1, 'The Slakrs', True)
     channel_id = channelInfo['channel_id']
 
+    # Send message
+    message_send(token1, channel_id, "hello")
+
+	# InputError when start > total messages
     with pytest.raises(InputError) as e:
         channel_messages(token1, channel_id, 50)
 
 def test_channel_messages_unauthorised():
+	# CASE 4: Authorised user is not a member of channel with channel_id
     user1 = auth_register("user1@gmail.com", '123!@asdf', 'user1', 'Smith')
     token1 = user1['token']
 
@@ -60,5 +74,9 @@ def test_channel_messages_unauthorised():
     channelInfo = channels_create(token1, 'The Slakrs', True)
     channel_id = channelInfo['channel_id']
 
-    with pytest.raises(AccesssError) as e:
+    # Send message
+    message_send(token1, channel_id, "hello")
+    
+	# AccessError when user sends message to channel they aren't a member of, user2
+    with pytest.raises(AccessError) as e:
         channel_messages(token2, channel_id, 0)

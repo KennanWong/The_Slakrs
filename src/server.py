f5e11aeb32ab14ed6cf0ae1d5f73a6a3fc2b1e14
KennanW <wongkennan@gmail.com>
2020-03-20 19:13:39 +1100

    wrote code for auth_login and auth_logout

--- src/server.py
+++ src/server.py
@@ -1,39 +1,60 @@
 import sys
+import re
+import hashlib
 from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
-import re
+
+LOGGED_ON = 1
+LOGGED_OFF = 0
+is_success = True
 
 users = [
 ]
-user = {
-    'u_id',
-    'email',
-    'name_last',
-    'name_first',
-    'handle_str',
-}
 
-def useres_rest():
+auth_data = [
+    # user_data :{
+    #     'u_id',
+    #     'email',
+    #     'password',
+    #     'token'
+    #     'status'
+    # }
+]
+
+def users_rest():
     global users
     users = []
+    global auth_data
+    auth_data = []
     return users
 
 
 # to get global users
-def getUserStore():
+def get_user_store():
     global users
     return users
 
+# to get gloabl auth_data store
+def get_auth_data_store():
+    global auth_data
+    return auth_data
+
 # to test if an email is valid, courtesy of geeksforgeeks.org
-def testEmail(email):
+def test_email(email):
     regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
     
     if (re.search(regex, email)):
         return email
     else:
-        raise InputError
+        raise InputError(description='Invalid Email')
+
+# to generate a token
+def generate_token(u_id):
+    return hashlib.sha256(str(u_id).encode()).hexdigest()
+
+
 
 def defaultHandler(err):
     response = err.get_response()
@@ -62,38 +83,150 @@ def echo():
         'data': data
     })
 
+
+# reset all users in a slack
+@APP.route("/users/reset", methods=['POST'])
+def users_reset():
+    users_rest()
+    return dumps({})
+
+
+'''
+#############################################################
+#                   AUTH_REGISTER                           #      
+#############################################################
+'''
+
+
 #register a user and add it to the userStore
 @APP.route("/auth/register", methods=['POST'])
 def auth_register():
-    usersStore = getUserStore()
+    users_store = get_user_store()
+    auth_store = get_auth_data_store()
     payload = request.get_json()
     handle = (payload['name_first']+payload['name_last'])
     if len(handle) > 24:
         handle = handle[0:20]
 
     #test if valid email    
-    email = testEmail(payload['email'])
+    email = test_email(payload['email'])
 
-
+    #test strength of password
+    if len(payload['password']) > 6:
+        password = payload['password']
+    else:
+         raise InputError (description='Password is too short')
     
-    new_user = {
-        'u_id': int(len(usersStore)+1),
-        'email': email,
-        'name_first': payload['name_first'],
-        'name_last': payload['name_last'],
-        'handle_str': handle.lower()
-    }
-    usersStore.append(new_user)
+    #test length of Last and First names
+    if 1 <= len(payload['name_first']) <= 50:
+        first_name = payload['name_first']
+    else:
+        raise InputError(description='Not a valid first name')
 
-    print(usersStore)
+    if 1 <= len(payload['name_last']) <= 50:
+        last_name = payload['name_last']
+    else:
+        raise InputError(description='Not a valid last name')
+
+    u_id = int(len(users_store)+1)
+    token = generate_token(u_id)
+
+    new_user = {
+        'u_id': u_id,
+        'email': email,
+        'name_first': first_name,
+        'name_last': last_name,
+        'handle_str': handle.lower(),
+    }
+
+    new_user_auth = {
+        'u_id' : u_id,
+        'email': email,
+        'password': password,
+        'token': token,
+        'status' : LOGGED_ON
+    }
+
+    #test if an email is alread taken
+    for i in users_store:
+        if i['email'] == email:
+            raise InputError(description='Email is already in use')
+
+    users_store.append(new_user)
+    auth_store.append(new_user_auth)
+
     return dumps({
-        'u_id':new_user['u_id'],
-        'token': 1
+        'u_id': u_id,
+       'token': token
     })
+
+'''
+#############################################################
+#                   AUTH_LOGIN                              #      
+#############################################################
+'''
+
+@APP.route("/auth/login", methods=['POST'])
+def auth_login():
+    auth_store = get_auth_data_store()
+    payload = request.get_json()
+
+    email = test_email(payload['email'])
+
+    emailMatch = 0  # if found = 1
+    passwordMatch = 0 # if match, password = 1
+
+    user_auth_data = {}
+
+    for i in auth_store:
+        if i['email'] == email:
+            emailMatch = 1
+            if i['status'] == LOGGED_OFF:
+                if i['password'] == payload['password']:
+                    user_auth_data = i
+                    i['status'] == LOGGED_ON
+                else:
+                    raise InputError(description="Incorrect password")
+            else: 
+                raise InputError(description="User already logged in")
+
+    if emailMatch == 0:
+        raise InputError(description="Email entered does not belong to a user")
+    
+    return dumps({
+        'u_id' : user_auth_data['u_id'],
+        'token' : user_auth_data['token']
+    })
+
+'''
+#############################################################
+#                   AUTH_LOGOUT                             #      
+#############################################################
+'''
+@APP.route("/auth/logout", methods=['POST'])
+def auth_logout():
+    auth_store = get_auth_data_store()
+    payload = request.get_json()
+
+
+    for i in auth_store:
+        if i['token'] == payload['token']:
+
+            if i['status'] == LOGGED_ON:
+
+                i['status'] = LOGGED_OFF
+ 
+                return dumps({
+                })
+
+    return dumps({
+    })
+
+
+
+
 
 
 if __name__ == "__main__":
     APP.run(port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
-
-
 

f59aa763254bbd977cfd09de050a3fa4434a6935
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-21 20:00:16 +1100

    channels_create finished

--- src/server.py
+++ src/server.py
@@ -4,6 +4,50 @@ from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
 
+
+
+channels_store = [
+    #new_channel_info
+    #{
+     #   'channel_id'
+      #  'name'
+       # 'is_public' 
+        #'members': {
+         #   u_id
+          #  name_first
+           # name_last
+        #}
+        #'owners': {
+         #   u_id
+          #  name_first
+           # name_last
+        #}
+        #'messages': {
+         #   message_id
+          #  u_id, message
+           # time_created
+            #reacts
+            #is_pinned
+
+        #}
+    #}
+]
+auth_data = [
+    user_data :{
+        'u_id',
+        'email',
+        'password',
+        'token'
+        'status'
+    }
+]
+
+
+
+def get_channel_data_store():
+    global channels_store
+    return channels_store
+
 def defaultHandler(err):
     response = err.get_response()
     print('response', err, err.get_response())
@@ -30,6 +74,113 @@ def echo():
     return dumps({
         'data': data
     })
+
+
+# reset all users in a slack
+@APP.route("/users/reset", methods=['POST'])
+def users_reset():
+    users_rest()
+    return dumps({})
+
+
+
+'''
+#############################################################
+#                   CHANNELS_CREATE                         #      
+#############################################################
+'''
+
+@APP.route("/channels/create", methods=['POST'])
+
+def channels_create():
+    auth_store = get_auth_data_store()
+    channel_store = get_channel_data_store()
+    payload = request.get_json()
+   
+
+    for i in auth_store:
+        if i['token'] == payload['token']:
+            channel_owner_info = {
+                'u_id': i['u_id']
+                'name_first': i['name_first']
+                'name_last': i['name_last']
+                'handle_str': i['handle_str']
+            }
+
+            if len(payload['name']) < 21:
+                name = payload['name']
+
+                if payload['is_public'] 
+                    new_channel_info =  {
+                        'channel_id': int(len(channel_store)+1)
+                        'name':  name
+                        'is_public': True
+                        'members':[]
+
+                        'owners':[]
+                           
+                        
+                        'messages': []
+
+                    }
+
+                else:
+                    new_channel_info = {
+                        'channel_id': int(len(channel_store)+1)
+                        'name': name
+                        'is_public': False
+                        'members':[]
+
+                        'owners':[]
+                           
+                        
+                        'messages': []
+
+                    }
+
+            else: 
+                raise InputError (description='Name is too long')
+                     
+    
+    new_channel_info['owners'].append(channel_owner_info)
+    channel_store.append(new_channel_info)
+   
+    return dumps ({
+        'channel_id': new_channel_info['channel_id']
+    })
+
+
+
+'''
+#############################################################
+#                   CHANNELS_LIST                           #      
+#############################################################
+'''
+
+def channels_list():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     APP.run(port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))

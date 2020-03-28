1b2bdf81918a086dbec6394d1ff3a9609799eb1d
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-28 17:35:08 +1100

    update

--- src/standup.py
+++ src/standup.py
@@ -18,16 +18,15 @@ def start(payload):
     user = get_user_token(payload['token'])
     channel = get_channel(payload['channel_id'])
 
-    
-    standup_message = []
-    standup_info = {}
-    
     if channel['is_active'] is False:
         if test_in_channel(user, channel) is True:
             if payload['length'] > 0:
                 length = payload['length']
                 channel['is_active'] = True
-                
+                timer = threading.Timer(length, end_standup)
+                timer.start()
+            
+
                 current_time = datetime.datetime.now().time()
                 time_finish = addSecs(current_time, length)
 
@@ -36,11 +35,16 @@ def start(payload):
 
     return time_finish
 
-def end_standup(payload):
 
-    length = payload['length']
-    timer = threading.Timer(length, start)
-    timer.start()
+#run this function where it collates all messages into one
+def end_standup(payload):
+    
+    messages = get_messages_store()
+    channel = get_channel(payload['channel_id'])
+    standup_message = []
+
+    if channel['is_active'] is True:
+        #unsure what to do here
 
 def active(payload):
     channel_store = get_channel_data_store()
@@ -60,19 +64,18 @@ def send(payload):
     channel_store = get_channel_data_store()
     messages = get_messages_store()
 
-    standup_message = []
-    standup_info = {}
-
     user = get_user_token(payload['token'])
     channel = get_channel(payload['channel_id'])
-
+    
     if channel['is_active'] is True:
         if test_in_channel(user, channel) is True:
             message = payload['message']
             if len(message) < 1000:
-                #unsure what to do here
-
-
+                standup_message = create_message()
+                standup_message['time_created'] = time
+                standup_message['u_id'] = user['u_id']
+                standup_message['message'] = message
+                standup_message['channel_id'] = payload['channel_id']
 
             else:
                 raise InputError(description='Message too long')
@@ -83,5 +86,11 @@ def send(payload):
         raise InputError(description='Active standup is not running')
 
 
+    messages.append(standup_message)
+    channel['messages'].append(standup_message)
+
     return 
     
+
+
+   

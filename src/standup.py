4a2b1798458b0a9b3270e229cb4be726632fe4d1
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-29 01:24:06 +1100

    update

--- src/standup.py
+++ src/standup.py
@@ -18,34 +18,53 @@ def start(payload):
     user = get_user_token(payload['token'])
     channel = get_channel(payload['channel_id'])
 
-    if channel['is_active'] is False:
+    standup = channel['standup']
+
+
+    if standup['is_active'] is False:
         if test_in_channel(user, channel) is True:
             if payload['length'] > 0:
                 length = payload['length']
-                channel['is_active'] = True
+                standup['is_active'] = True
                 timer = threading.Timer(length, end_standup)
                 timer.start()
             
-
                 current_time = datetime.datetime.now().time()
                 time_finish = addSecs(current_time, length)
+                standup['time_finish'] = time_finish
+            
+            else:
+                raise InputError(description='Negative length is invalid')
+        
+        else:
+            raise AccessError(description='User not in channel')
 
     else:
         raise InputError(description='An active standup is running')
+
 
     return time_finish
 
 
 #run this function where it collates all messages into one
 def end_standup(payload):
-    
+
+    channel_store = get_channel_data_store()
     messages = get_messages_store()
-    channel = get_channel(payload['channel_id'])
+
     standup_message = []
 
-    if channel['is_active'] is True:
-        #unsure what to do here
-
+    channel = get_channel(payload['channel_id'])
+    standup = channel['standup']
+    
+    standup_message.append(standup['messages'])
+    channel['messages'].append(standup['messagesß'])
+    channel_store.append(standup['messages'])
+        
+#############################################################
+#                   STANDUP_ACTIVE                          #      
+#############################################################
+        
 def active(payload):
     channel_store = get_channel_data_store()
     messages = get_messages_store()
@@ -53,12 +72,22 @@ def active(payload):
     user = get_user_token(payload['token'])
     channel = get_channel(payload['channel_id'])
 
-    if channel['is_active'] is True:
-        length = payload['length']
-        current_time = datetime.datetime.now().time()
-        time_finish = addSecs(current_time, length)
 
-        return True, time_finish
+    standup = channel['standup']
+
+    if standup['is_active'] is True:
+        standup_info = {
+            'is_active': True,
+            'time_finish': standup['time_finish']
+        }
+
+    return standup_info
+
+
+
+#############################################################
+#                   STANDUP_SEND                            #      
+#############################################################
 
 def send(payload):
     channel_store = get_channel_data_store()
@@ -67,29 +96,28 @@ def send(payload):
     user = get_user_token(payload['token'])
     channel = get_channel(payload['channel_id'])
     
-    if channel['is_active'] is True:
+    standup = channel['standup']
+
+    if standup['is_active'] is True:
         if test_in_channel(user, channel) is True:
             message = payload['message']
             if len(message) < 1000:
-                standup_message = create_message()
-                standup_message['time_created'] = time
-                standup_message['u_id'] = user['u_id']
-                standup_message['message'] = message
-                standup_message['channel_id'] = payload['channel_id']
-
+                new_message = {
+                    'Name_first': user['name_first'],
+                    'Message': message
+                }
+                
             else:
                 raise InputError(description='Message too long')
         else:
-            raise AccessError(description = 'User is not part of channel')
+            raise AccessError(description='User is not part of channel')
 
     else:
         raise InputError(description='Active standup is not running')
 
+    standup['messages'].append(new_message)
 
-    messages.append(standup_message)
-    channel['messages'].append(standup_message)
-
-    return 
+    return
     
 
 

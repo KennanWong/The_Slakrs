ed1da76afca8efbe1918209d6305b5500d30e55f
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-28 19:43:52 +1100

    Looked at permissions cases

--- src/channel_join.py
+++ src/channel_join.py
@@ -9,18 +9,20 @@ def channel_join(token, channel_id):
     # Check if channel exists using helper function
     channel = get_channel(channel_id)
     
+    if user['slack_owner'] == True:
     # InputError if channel_id does not refer to a valid channel
     if channel is None:
         raise InputError(description='Invalid channel_id')
-
-    # AccessError when attempting to join a private channel
-    if channel['is_public'] == False: #or if not channel['is_public']
-        raise AccessError(description='Cannot join a private channel')
+    
+    #or if not channel['is_public']
+    if channel['is_public'] == True and user['permission_id'] == 1:
+        channel['owner_members'].append(user)
+    elif channel['is_public'] == True and user['permission_id'] == 2:
+        channel['all_members'].append(user)
+    elif channel['is_public'] == False and user['permission_id'] == 1:
+        channel['owner_members'].append(user)
     else:
-        # User joining channel
-        for user in channel['all_members']:
-            if user['u_id'] == u_id:
-                channel['all_members'].append(user)
-    
+        # AccessError when attempting to join a private channel
+        raise AccessError(description='Cannot join a private channel')
+
     return {}
-    

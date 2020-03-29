d2ed4b4a09e6440ea724289b54fdab05348f9c1d
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-29 20:27:55 +1100

    Edited

--- src/channel_messages.py
+++ src/channel_messages.py
@@ -5,21 +5,26 @@ from error import InputError, AccessError
 def channel_messages(token, channel_id, start):
     auth_store = get_auth_data_store
 
+    u_id = user_id_from_token(token)
     # Check if channel exists using helper function
     channel = get_channel(channel_id)
 
     # InputError when we try to get details of an invalid channelID
-    if channel is None:
-        raise InputError(description='Invalid channel_id')
+    #if channel is None:
+    #    raise InputError(description='Invalid channel_id')
 
     # Check if authorised user is a member of the channel, AccessError if not
-    if u_id not in channel['members']:
-        raise AccessError(description = 'Authorised user is not a member of the channel')
+    if not test_in_channel(u_id, channel):
+        raise AccessError(description='Authorised user is not a member of the channel')
       
     # InputError when start is greater than or equal to the total number of messages in the channel
-    if start >= len(messages):
+    if start >= len(channel['messages']):
         raise InputError(description='Start is greater than or equal to the total number of messages in the channel')
-
+    
+    # Incorrect indexing
+    if start < 0 or start > len(channel['messages']):
+        raise InputError(description='Incorrect indexing')
+    
     if not isinstance(start, int):
         raise InputError(description='Start is not of type integer')
 
@@ -28,27 +33,29 @@ def channel_messages(token, channel_id, start):
         return {
             "messages": [],
             "start": start,
-            "end": end
+            "end": start
         }
     
-    message_list = []
-    for messages in channel['messages']:
-        for reacts in message['reacts']:
-            for users_reacted in react['u_ids']:
+    messages_list = []
+    messages_dict = {}
+    if start + 50 > len(channel['messages']):
+        interval = start + 50
+    else:
+        interval = len(channel['messages'])
+    for messages in channel['messages'][start: interval]:
+        messages_dict = {
+            'message_id': messages['message_id'],
+            'u_id': messages['u_id'],
+            'message': messages['message'],
+            'time_created': messages['time_created'],
+            'reacts': messages['reacts'],
+            'is_pinned': messages['is_pinned']
+        }
+        for reacts in messages['reacts']:
+            for users_reacted in reacts['u_ids']:
                 if u_id in reacts['u_id']:
                     reacts['is_this_user_reacted'] == True
-                else:
-                    reacts['is_this_user_reacted'] == False
-
-    messsage_dict = {
-        'message_id': messages['message_id'],
-        'u_id': messages['u_id'],
-        'message': messages['message'],
-        'time_created': messages['time_created'],
-        'reacts': messages['reacts'],
-        'is_pinned': messages['is_pinned']
-    }
-    message_list.append(message_dict)
+        messages_list.append(messages_dict)
 
     if start < len(channel['messages']):
         if len(channel['messages'][start:]) > 50:
@@ -58,10 +65,9 @@ def channel_messages(token, channel_id, start):
                 "start": start,
                 "end": end
             }
-        else:
-            end = -1
+        if len(channel['messages'][start:]) < 50:
             return {
                 "messages": messages_list,
                 "start": start,
                 "end": -1
-            }       
+            }

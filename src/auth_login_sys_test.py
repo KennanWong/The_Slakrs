5bd2674ba4771ef55faea1f64732eb958d766ce1
KennanW <wongkennan@gmail.com>
2020-03-31 02:24:38 +1100

    pylinted

--- src/auth_login_sys_test.py
+++ src/auth_login_sys_test.py
@@ -1,16 +1,14 @@
-import pytest
+'''
+Pytest file to test auth_login on a system level
+'''
+
 import urllib
 import json
 import flask
 from urllib.error import HTTPError
+import pytest
 
-import server
-import auth
 from system_helper_functions import reg_user1, reset_workspace, logout_user1
-from other import workspace_reset
-from data_stores import get_auth_data_store, reset_auth_store
-from helper_functions import get_user_token
-from error import InputError
 
 
 #############################################################
@@ -20,11 +18,14 @@ from error import InputError
 BASE_URL = 'http://127.0.0.1:8080'
 
 def test_login():
+    '''
+    Test valid case of test_login
+    '''
     reset_workspace()
 
     # Register a user
     response = reg_user1()
-    
+
     # Logout that user
     logout_user1(response['token'])
 
@@ -34,9 +35,9 @@ def test_login():
         'password': 'Wong123'
     }).encode('utf-8')
     req = urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/auth/login", 
-        data = data, 
-        headers = {'Content-Type':'application/json'}
+        f"{BASE_URL}/auth/login",
+        data=data,
+        headers={'Content-Type':'application/json'}
     ))
     payload = json.load(req)
 
@@ -46,11 +47,15 @@ def test_login():
 
 
 def test_invalid_email():
+    '''
+    Test auth/login on an invalid email
+    '''
+
     reset_workspace()
-    
+
     # Register a user
     response = reg_user1()
-    
+
     # Logout that user
     logout_user1(response['token'])
 
@@ -58,20 +63,24 @@ def test_invalid_email():
         'email': 'Kennan@.com',
         'password': 'Wong123'
     }).encode('utf-8')
-    
+
     with pytest.raises(HTTPError):
-        req = urllib.request.urlopen(urllib.request.Request(
-            f"{BASE_URL}/auth/login", 
-            data = data, 
-            headers = {'Content-Type':'application/json'}
+        urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/login",
+            data=data,
+            headers={'Content-Type':'application/json'}
         ))
 
 def test_wrong_email():
+    '''
+    Test auth/login with an incorrect email
+    '''
+
     reset_workspace()
-    
+
     # Register a user
     response = reg_user1()
-    
+
     # Logout that user
     logout_user1(response['token'])
 
@@ -79,20 +88,24 @@ def test_wrong_email():
         'email': 'Kennand@gmail.com',
         'password': 'Wong123'
     }).encode('utf-8')
-    
+
     with pytest.raises(HTTPError):
-        req = urllib.request.urlopen(urllib.request.Request(
-            f"{BASE_URL}/auth/login", 
-            data = data, 
-            headers = {'Content-Type':'application/json'}
+        urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/login",
+            data=data,
+            headers={'Content-Type':'application/json'}
         ))
 
 def test_wrong_pass():
+    '''
+    Test auth/login with an incorrect password
+    '''
+
     reset_workspace()
-    
+
     # Register a user
     response = reg_user1()
-    
+
     # Logout that user
     logout_user1(response['token'])
 
@@ -100,12 +113,10 @@ def test_wrong_pass():
         'email': 'Kennan@gmail.com',
         'password': 'Wong321'
     }).encode('utf-8')
-    
+
     with pytest.raises(HTTPError):
-        req = urllib.request.urlopen(urllib.request.Request(
-            f"{BASE_URL}/auth/login", 
-            data = data, 
-            headers = {'Content-Type':'application/json'}
+        urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/login",
+            data=data,
+            headers={'Content-Type':'application/json'}
         ))
-
-

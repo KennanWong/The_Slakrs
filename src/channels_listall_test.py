cda27b74342d1737431d8400839a38cc76a24d79
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-30 20:22:09 +1100

    update

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -9,28 +9,6 @@ from error import InputError, AccessError
 from data_stores import get_channel_data_store, get_messages_store
 
 
-#def test_listall_and_details():
- #   results = auth_register("guest123@gmail.com", '123!Asdf', 'John', 'Smith')
-  # u_id1 = results['u_id']
-#
- #   result2 = auth_register("sidsat@gmail.com", '123!Asdf', 'Sid', 'Sat')
-  #  token2 = results['token']
-   # u_id2 = results['u_id']
-#
- #   channel_info1 = channels_create(token1, 'Slakrs', True)
-  #  channel_info2 = channels_create(token2, 'Kings Landing', True)
-#
- #   all_channels = channels_listall(token2)
-#
- #   flag = 0
-  #  j = 0
-#
- #   for i in all_channels:
-  #      if (channel_info1 or channel_info2) == all_channels[j]:
-   #         flag = 1   
-    #assert flag == 1
-
-
 #############################################################
 #                   CHANNELS_LISTALL                        #
 #############################################################

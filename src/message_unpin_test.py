a66d2f833c143efccbab9c6052b84e8ddd9bf740
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-30 21:53:18 +1100

    update

--- src/message_unpin_test.py
+++ src/message_unpin_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 import message
-#import channel
+import channel
 from other import workspace_reset
 from test_helper_functions import reg_user1, register_and_create, send_msg1, reg_user2
 from error import InputError, AccessError
@@ -116,29 +116,28 @@ def test_unauthor_member():
         })  
 
 
-#def test_unauth_owner():
+def test_unauth_owner():
 
     #user is not an owner
 
- #   workspace_reset()
+    workspace_reset()
 
-  #  ret = register_and_create()
-  #  user = ret['user']
-  #  channel1 = ret['channel']
+    ret = register_and_create()
+    user = ret['user']
+    channel1 = ret['channel']
 
-  #  user2 = reg_user2()
+    user2 = reg_user2()
 
-  #  channel.invite({
-  #      'token': user['token'],
-  #      'channel_id': channel1['channel_id'],
-  #      'u_id': user2['u_id']
-  #  })
+    token = user['token']
+    channel_id = channel1['channel_id']
+    u_id = user2['u_id']
+    channel.invite(token, channel_id, u_id)
 
-   # msg1 = send_msg1(user, channel1)
+    msg1 = send_msg1(user, channel1)
     
-   # with pytest.raises(InputError):
-    #    message.pin({
-   #         'token': user2['token'],
-    #        'message_id': msg1['message_id'],
-    #    })  
+    with pytest.raises(InputError):
+        message.pin({
+            'token': user2['token'],
+            'message_id': msg1['message_id'],
+        })  
 

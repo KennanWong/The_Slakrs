2a0859046421c562b3301edad83a6fd5e57e9f48
Siddharth Satyavolu <z5257600@unsw.edu.au>
2020-03-30 20:21:26 +1100

    update

--- src/channels_list_test.py
+++ src/channels_list_test.py
@@ -9,18 +9,6 @@ from error import InputError, AccessError
 from data_stores import get_channel_data_store, get_messages_store
 
 
-#def test_list_channel_one():
-#    results = auth_register("guest123@gmail.com", '123!Asdf', 'John', 'Smith')
-#    token1 = results['token']
-#    u_id1 = results['u_id']
-
-    #returns channel_id
-#    channel_info1 = channels_create(token1, 'Slakrs', True)
-
- #   my_channel_id1 = channel_info1['channel_id']
-
-
- #   assert (channels_list(token1)["channels"][0]['channel_id']) == my_channel_id1
 
 #############################################################
 #                   CHANNELS_LIST                           #

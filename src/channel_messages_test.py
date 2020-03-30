82468de72d24c5f757c37e3f2e9f68b24390ed4b
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-31 02:48:14 +1100

    Almost done

--- src/channel_messages_test.py
+++ src/channel_messages_test.py
@@ -82,3 +82,91 @@ def test_channel_messages_unauthorised():
     # user2 isn't a member
     with pytest.raises(AccessError) as e:
         channel_messages(token2, channel_id, 0)
+
+
+import pytest
+import channel
+import message
+from other import workspace_reset
+from test_helper_functions import reg_user1, reg_user2, register_and_create
+from error import InputError, AccessError
+
+def test_channel_messages_clean():
+    workspace_reset()
+    ret = register_and_create()
+    user1 = ret['user']
+    token1 = user1['token']
+    channelInfo = ret['channel']
+    channel_id = channelInfo['channel_id']
+
+    # No messages in channel
+    assert channel.messages(token1, channel_id, 0)["messages"] == []
+    assert channel.messages(token1, channel_id, 0)["start"] == 0
+    assert channel.messages(token1, channel_id, 0)["end"] == -1
+
+def test_channel_messages_invalid_channel():
+    workspace_reset()
+    ret = register_and_create()
+    user1 = ret['user']
+    token1 = user1['token']
+    channelInfo = ret['channel']
+    channel_id = channelInfo['channel_id']
+    
+    payload = {
+        'token': token1,
+        'channel_id': channel_id,
+        'message': "hello"
+    }
+
+    # Send message
+    message.send(payload)
+
+    # InputError when we try to check messages in an invalid channel
+    # Invalid channel_id = 100
+    with pytest.raises(InputError) as e:
+        channel.messages(token1, 100, 0)
+
+def test_channel_messages_start_excess():
+    workspace_reset()
+    ret = register_and_create()
+    user1 = ret['user']
+    token1 = user1['token']
+    channelInfo = ret['channel']
+    channel_id = channelInfo['channel_id']	
+
+    payload = {
+        'token': token1,
+        'channel_id': channel_id,
+        'message': "hello"
+    }
+
+    # Send message
+    message.send(payload)
+    
+    # InputError when start > total messages
+    with pytest.raises(InputError) as e:
+        channel.messages(token1, channel_id, 50)
+
+def test_channel_messages_unauthorised():
+    workspace_reset()
+    ret = register_and_create()
+    user1 = ret['user']
+    token1 = user1['token']
+    channelInfo = ret['channel']
+    channel_id = channelInfo['channel_id']
+    user2 = reg_user2()
+    token2 = user2['token']
+    
+    payload = {
+        'token': token1,
+	    'channel_id': channel_id,
+	    'message': "hello"
+	}
+    
+    # Send message
+    message.send(payload)
+
+    # AccessError when user sends message to channel they aren't a member of
+    # user2 isn't a member
+    with pytest.raises(AccessError) as e:
+        channel.messages(token2, channel_id, 0)

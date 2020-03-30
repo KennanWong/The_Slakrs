9817b791592f8436814b71a85dd606aaefbef31a
KennanW <wongkennan@gmail.com>
2020-03-30 23:52:41 +1100

    addded error cases

--- src/auth_register_sys_test.py
+++ src/auth_register_sys_test.py
@@ -7,6 +7,7 @@ import pytest
 import urllib
 import json
 import flask
+from urllib.error import HTTPError
 
 import server
 import auth
@@ -26,7 +27,6 @@ BASE_URL = 'http://127.0.0.1:8080'
 def test_register1():
     reset_workspace()
 
-    
     data = json.dumps({
         'email' : 'Kennan@gmail.com',
         'password': 'Wong123',
@@ -43,25 +43,122 @@ def test_register1():
     # asserts that the u_id's of the one return matches the user found
     assert payload['u_id'] == 1
 
-    
 
-'''
-def test_sys_invalid_email():
-    reset = req = urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data = [],  
-        headers = {'Content-Type':'application/json'}
-    ))
+
+def test_invalid_email():
+    reset_workspace()
     data = json.dumps({
         'email' : 'Kennan@.com',
         'password': 'Wong123',
         'name_first': 'Kennan',
         'name_last': 'Wong'
     }).encode('utf-8')
-    with pytest.raises(InputError):
+    with pytest.raises(HTTPError):
         req = urllib.request.urlopen(urllib.request.Request(
             f"{BASE_URL}/auth/register", 
             data = data, 
             headers = {'Content-Type':'application/json'}
         ))
-'''
+
+def test_email_used():
+    reset_workspace()
+
+    # Register user first
+    data = json.dumps({
+        'email' : 'Kennan@gmail.com',
+        'password': 'Wong123',
+        'name_first': 'Kennan',
+        'name_last': 'Wong'
+    }).encode('utf-8')
+    urllib.request.urlopen(urllib.request.Request(
+        f"{BASE_URL}/auth/register", 
+        data = data, 
+        headers = {'Content-Type':'application/json'}
+    ))
+
+    # Attempt to register new user with the same email
+    data2 = json.dumps({
+        'email' : 'Kennan@gmail.com',
+        'password': 'Wong123',
+        'name_first': 'Ken',
+        'name_last': 'Wong'
+    }).encode('utf-8')
+    with pytest.raises(HTTPError):
+        req = urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/register", 
+            data = data, 
+            headers = {'Content-Type':'application/json'}
+        ))
+
+def test_short_pass():
+    reset_workspace()
+
+    data = json.dumps({
+        'email' : 'Kennan@gmail.com',
+        'password': 'Wong',
+        'name_first': 'Kennan',
+        'name_last': 'Wong'
+    }).encode('utf-8')
+    with pytest.raises(HTTPError):
+        req = urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/register", 
+            data = data, 
+            headers = {'Content-Type':'application/json'}
+        ))
+
+def test_short_first_name():
+    reset_workspace()
+
+    data = json.dumps({
+        'email' : 'Kennan@gmail.com',
+        'password': 'Wong123',
+        'name_first': 'K',
+        'name_last': 'Wong'
+    }).encode('utf-8')
+    with pytest.raises(HTTPError):
+        req = urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/register", 
+            data = data, 
+            headers = {'Content-Type':'application/json'}
+        ))
+
+def test_short_last_name():
+    reset_workspace()
+
+    data = json.dumps({
+        'email' : 'Kennan@gmail.com',
+        'password': 'Wong123',
+        'name_first': 'Kennan',
+        'name_last': 'W'
+    }).encode('utf-8')
+    with pytest.raises(HTTPError):
+        req = urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/register", 
+            data = data, 
+            headers = {'Content-Type':'application/json'}
+        ))
+
+
+def test_register_double():
+    reset_workspace()
+
+    # Register user first
+    data = json.dumps({
+        'email' : 'Kennan@gmail.com',
+        'password': 'Wong123',
+        'name_first': 'Kennan',
+        'name_last': 'Wong'
+    }).encode('utf-8')
+    urllib.request.urlopen(urllib.request.Request(
+        f"{BASE_URL}/auth/register", 
+        data = data, 
+        headers = {'Content-Type':'application/json'}
+    ))
+
+    with pytest.raises(HTTPError):
+        req = urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/auth/register", 
+            data = data, 
+            headers = {'Content-Type':'application/json'}
+        ))
+

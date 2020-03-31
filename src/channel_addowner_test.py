cb9a49e0636ba21b248739cad77bb8c09619bd30
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-31 15:19:07 +1100

    Finalised

--- src/channel_addowner_test.py
+++ src/channel_addowner_test.py
@@ -1,24 +1,29 @@
-
-
+'This is the integration test file for channel_addowner'
 
 import pytest
 import channel
 from other import workspace_reset
-from test_helper_functions import reg_user1, reg_user2, reg_user3, register_and_create
+from test_helper_functions import reg_user2, reg_user3, register_and_create
 from error import InputError, AccessError
 
+# pylint: disable=W0612
+# pylint: disable=C0103
+
+# pylint compliant
+
 def test_channel_add_owner():
+    'Normal case'
     workspace_reset()
     ret = register_and_create()
     user1 = ret['user']
     token1 = user1['token']
     u_id1 = user1['u_id']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
     u_id2 = user2['u_id']
+
+    channel_id = channel_info['channel_id']
 
     channel.join(token2, channel_id)
 
@@ -30,45 +35,46 @@ def test_channel_add_owner():
         "name_first": 'Kennan',
         "name_last": 'Wong'
     },
-    {
-        "u_id": u_id2,
-        "name_first": 'Cindy',
-        "name_last": 'Tran'
-    }]
+                                                                    {
+                                                                        "u_id": u_id2,
+                                                                        "name_first": 'Cindy',
+                                                                        "name_last": 'Tran'
+                                                                    }]
 
 def test_already_owner():
+    'Already an owner case'
     workspace_reset()
     ret = register_and_create()
     user1 = ret['user']
     token1 = user1['token']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
     u_id2 = user2['u_id']
 
+    channel_id = channel_info['channel_id']
+
     channel.join(token2, channel_id)
 
     channel.addowner(token1, channel_id, u_id2)
-    
+
     # InputError because user2 is already an owner
     with pytest.raises(InputError):
         channel.addowner(token1, channel_id, u_id2)
 
 def test_not_owner():
+    'Non-owner case'
     workspace_reset()
     ret = register_and_create()
     user1 = ret['user']
-    u_id1 = user1['u_id']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
     user3 = reg_user3()
     token3 = user3['token']
     u_id3 = user3['u_id']
+
+    channel_id = channel_info['channel_id']
 
     channel.join(token3, channel_id)
 
@@ -77,17 +83,19 @@ def test_not_owner():
         channel.addowner(token2, channel_id, u_id3)
 
 def test_invalid_channel():
+    'Invalid channel case'
     workspace_reset()
+
     ret = register_and_create()
     user1 = ret['user']
     token1 = user1['token']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
     u_id2 = user2['u_id']
-    
+
+    channel_id = channel_info['channel_id']
+
     channel.join(token2, channel_id)
     channel.addowner(token1, channel_id, u_id2)
 

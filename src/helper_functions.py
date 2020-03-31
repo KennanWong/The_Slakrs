6a3325a9136dc859fa4807ccd30db7862dc45e75
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-31 15:48:37 +1100

    Finalised

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -140,31 +140,6 @@ def addSecs(tm, secs):
     return fulldate.time()
     return False
 
-# Function to check valid userID
-def is_valid_user_id(u_id):
-    auth_store = get_auth_data_store()
-    for user in auth_store:
-        if user['u_id'] == u_id:
-            return 1
-    else:
-        raise InputError(description='Invalid u_id')
-
-# Function to get userID from token
-def user_id_from_token(token):
-    user = get_user_token(token)
-    return user['u_id']
-
-# Fucntion to get details of a user
-def user_details(u_id):
-    auth_store = get_auth_data_store()
-    for user in auth_store:
-        if u_id == user['u_id']:
-            return {
-                'u_id': u_id,
-                'name_first': user['name_first'],
-                'name_last': user['name_last']
-            }
-    return False
 
 # function to format a list of dictionaries into a members data type
 
@@ -181,14 +156,6 @@ def format_to_members(members):
         }
         members.append(add)
     return members
-
-def user_id_from_token():
-    auth_store = get_auth_data_store
-    for user in auth_store:
-        if token == user['token']:
-            return user['u_id']
-    else:
-        raise InputError(description='Could not find u_id')
 
 def get_message_count():
     '''

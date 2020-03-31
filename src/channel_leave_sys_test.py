7190b912c2790809a6a2c431b6262298a7819bfe
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-31 15:21:32 +1100

    Finalised

---
+++ src/channel_leave_sys_test.py
@@ -0,0 +1,88 @@
+'This is the system test for channel_leave'
+
+# pylint: disable=W0611
+# pylint: disable=C0412
+
+# pylint compliant
+
+import urllib
+import json
+from urllib.error import HTTPError
+import flask
+import pytest
+
+import server
+import channel
+from system_helper_functions import reg_user1, reg_user2, create_ch1, reset_workspace
+
+BASE_URL = 'http://127.0.0.1:8080'
+
+def test_channel_leave():
+    'Normal leave case'
+    # channel_leave (POST)
+    reset_workspace()
+
+    user1 = reg_user1()
+    token1 = user1['token']
+
+    channel_info = create_ch1(user1)
+    channel_id = channel_info['channel_id']
+
+    data = json.dumps({
+        'token': token1,
+        'channel_id': channel_id,
+    }).encode('utf-8')
+    req = urllib.request.urlopen(urllib.request.Request(
+        f"{BASE_URL}/channel/leave",
+        data=data,
+        headers={'Content-Type': 'application/json'}
+    ))
+    payload = json.load(req)
+
+    assert payload == {}
+
+def test_channel_leave_invalid_channel():
+    'Invalid channel case'
+    reset_workspace()
+
+    user1 = reg_user1()
+    token1 = user1['token']
+
+    #channel_info = create_ch1(user1)
+
+    # User attempting to leave an invalid channel
+    # Invalid channel_id = 100
+    data = json.dumps({
+        'token': token1,
+        'channel_id': 100
+    }).encode('utf-8')
+
+    with pytest.raises(HTTPError):
+        urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/channel/leave",
+            data=data,
+            headers={'Content-Type': 'application/json'}
+        ))
+
+def test_channel_leave_unauthorised():
+    'User is not a member case'
+    reset_workspace()
+
+    #user1 = reg_user1()
+    user2 = reg_user2()
+    token2 = user2['token']
+
+    #channel_info = create_ch1(user1)
+
+    # Attempt for user to leave channel they aren't a member of (user2)
+    data = json.dumps({
+        'token': token2,
+        'channel_id': 100
+    }).encode('utf-8')
+
+    with pytest.raises(HTTPError):
+        urllib.request.urlopen(urllib.request.Request(
+            f"{BASE_URL}/channel/leave",
+            data=data,
+            headers={'Content-Type': 'application/json'}
+        ))

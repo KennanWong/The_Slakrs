5b2d5821b7bae6d21e5f6c99c44a4784cc1a16d5
Harjas Ahuja <z5260126@unsw.edu.au>
2020-03-31 15:19:19 +1100

    Finalised

--- src/channel_removeowner_test.py
+++ src/channel_removeowner_test.py
@@ -1,80 +1,98 @@
-
+'This is the integration test file for channel_removeowner'
 
 import pytest
 import channel
 from other import workspace_reset
-from test_helper_functions import reg_user1, reg_user2, register_and_create
+from test_helper_functions import reg_user2, register_and_create
 from error import InputError, AccessError
 
+# pylint: disable=W0612
+# pylint: disable=C0103
+
+# pylint compliant
+
 def test_channel_remove_owner():
+    'Normal case'
     workspace_reset()
+
     ret = register_and_create()
     user1 = ret['user']
     token1 = user1['token']
     u_id1 = user1['u_id']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
     u_id2 = user2['u_id']
 
+    channel_id = channel_info['channel_id']
+
     channel.join(token2, channel_id)
     channel.addowner(token1, channel_id, u_id2)
+
+    # User2 removes user1 as owner
     channel.removeowner(token2, channel_id, u_id1)
 
-    owners = channel.details(token2, channel_id)['owner_members']
+    assert channel.details(token1, channel_id)['owner_members'] == [{
+        "u_id": u_id2,
+        "name_first": 'Cindy',
+        "name_last": 'Tran'
+    }]
 
 def test_invalid_channel2():
+    'Invalid channel case'
     workspace_reset()
+
     ret = register_and_create()
     user1 = ret['user']
     token1 = user1['token']
     u_id1 = user1['u_id']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
     u_id2 = user2['u_id']
-    
+
+    channel_id = channel_info['channel_id']
+
     channel.join(token2, channel_id)
     channel.addowner(token1, channel_id, u_id2)
-    
+
     # Invalid channel_id = 100
-    with pytest.raises(InputError):
+    with pytest.raises(InputError) as e:
         channel.removeowner(token2, 100, u_id1)
-    
+
 def test_userid_not_owner():
+    'User is not an owner case'
     workspace_reset()
+
     ret = register_and_create()
     user1 = ret['user']
     token1 = user1['token']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    channel_info = ret['channel']
     user2 = reg_user2()
     token2 = user2['token']
-    u_id2 = user2['u_id']   
-    
+    u_id2 = user2['u_id']
+
+    channel_id = channel_info['channel_id']
+
     channel.join(token2, channel_id)
     channel.addowner(token1, channel_id, u_id2)
 
     # Invalid user_id = 100
-    with pytest.raises(InputError):
+    with pytest.raises(InputError) as e:
         channel.removeowner(token2, channel_id, 100)
 
 def test_not_owner2():
+    'Non-owner case'
     workspace_reset()
+
     ret = register_and_create()
-    user = ret['user']
-    channelInfo = ret['channel']
-    channel_id = channelInfo['channel_id']
-    
+    user1 = ret['user']
+    u_id1 = user1['u_id']
+    channel_info = ret['channel']
     user2 = reg_user2()
-    token = user['token']
-    u_id = user['u_id']
-    token2 = user2['token'] 
-    
-    with pytest.raises(AccessError):
-        channel.removeowner(token2, channel_id, u_id)
+    token2 = user2['token']
+
+    channel_id = channel_info['channel_id']
+
+    with pytest.raises(AccessError) as e:
+        channel.removeowner(token2, channel_id, u_id1)

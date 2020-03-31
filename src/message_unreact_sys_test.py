77c3587ea804272523132661ba72dc334d521174
KennanW <wongkennan@gmail.com>
2020-03-31 14:26:20 +1100

    tried to fix syntax error

--- src/message_unreact_sys_test.py
+++ src/message_unreact_sys_test.py
@@ -11,7 +11,7 @@ from system_helper_functions import reg_user1, reset_workspace, create_ch1
 from system_helper_functions import reg_user2, send_msg1, react_to_msg
 
 #############################################################
-#                   MESSAGE_UNREACT                           #
+#                   MESSAGE_UNREACT                         #
 #############################################################
 
 BASE_URL = 'http://127.0.0.1:8080'

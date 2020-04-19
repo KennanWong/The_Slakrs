d6d514ab917bde12fe9014c1bfe9ddff2ec73bfa
Harjas Ahuja <z5260126@unsw.edu.au>
2020-04-19 18:16:16 +1000

    Finished

--- src/channel_leave_sys_test.py
+++ src/channel_leave_sys_test.py
@@ -17,7 +17,7 @@ from system_helper_functions import reg_user1, reg_user2, create_ch1, reset_work
 
 # channel_leave (POST)
 
-BASE_URL = 'http://127.0.0.1:4000'
+BASE_URL = 'http://127.0.0.1:8080'
 
 def test_channel_leave():
     'Normal leave case'

a446a76e0e2c6b7426e235071bec3a8110a5a21b
Harjas Ahuja <z5260126@unsw.edu.au>
2020-04-19 16:12:16 +1000

    Update

--- src/http_tests.py
+++ src/http_tests.py
@@ -1,52 +1,40 @@
 import urllib.request
 import json
+from test_helper_functions import reg_user1, reg_user2, register_and_create
 import pytest
-import flask
-
-import server
-import auth
-from system_helper_functions import reg_user1, reg_user2, create_ch1, reset_workspace
-from other import workspace_reset
-#from data_stores import get_auth_data_store, reset_auth_store
-#from helper_functions import get_user_token
-from error import InputError
 
 BASE_URL = 'http://127.0.0.1:4999'
 
+ret = register_and_create()
+user1 = ret['user']
+channelInfo = ret['channel']
+channel_id = channelInfo['channel_id']
+user2 = reg_user2()
+
+token = user1['token']
+u_id = user1['u_id']
+token2 = user2['token']
+
 def test_channel_invite():
 # channel_invite (POST)
-    reset_workspace()
-    
-    # Register users
-    response1 = reg_user1()
-    response2 = reg_user2()
-    token1 = response1['token']
-    u_id2 = response2['u_id']
-    channelInfo = create_ch1()
-    channel_id = channelInfo['channel_id']
-
-    # Attempt to invite user2
     data = json.dumps({
-        'token': token1,
+        'token': token,
         'channel_id': channel_id,
-        'u_id': u_id2
+        'u_id': u_id
     }).encode('utf-8')
-    req = urllib.request.urlopen(urllib.request.Request(
+
+    req = urllib.request.Request(
         f"{BASE_URL}/channel/invite",
         data=data,
         headers={'Content-Type': 'application/json'}
-    ))
-    payload = json.load(req)
+    )
+    response = urllib.request.urlopen(req)
+    payload = json.load(response)
 
     assert payload == {}
 
 def test_channel_details():
 # channeL_details (GET)
-    urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data=[],
-        headers={'Content-Type': 'application/json'}
-    ))
     response = urllib.request.urlopen(f"{BASE_URL}/channel/details")
     payload = json.load(response)
     assert payload == {
@@ -57,11 +45,6 @@ def test_channel_details():
 '''
 def test_channel_messages():
 # channel_messages (GET)
-    urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data=[],
-        headers={'Content-Type': 'application/json'}
-    ))
     response = urllib.request.urlopen(f"{BASE_URL}/channel/messsages")
     payload = json.load(response)
     assert payload == {
@@ -72,15 +55,11 @@ def test_channel_messages():
 '''
 def test_channel_leave():
 # channel_leave (POST)
-    urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data=[],
-        headers={'Content-Type': 'application/json'}
-    ))
     data = json.dumps({
-        'token': token2, # user2 leaving
+        'token': token2, # second user leaving
         'channel_id': channel_id,
     }).encode('utf-8')
+
     req = urllib.request.Request(
         f"{BASE_URL}/channel/leave",
         data=data,
@@ -93,15 +72,11 @@ def test_channel_leave():
 
 def test_channel_join():
 # channnel_join (POST)
-    urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data=[],
-        headers={'Content-Type': 'application/json'}
-    ))
     data = json.dumps({
-        'token': token,
+        'token': token2, # second user joining
         'channel_id': channel_id,
     }).encode('utf-8')
+
     req = urllib.request.Request(
         f"{BASE_URL}/channel/join",
         data=data,
@@ -114,16 +89,12 @@ def test_channel_join():
 
 def test_channel_addowner():
 # channel_addowner (POST)
-    urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data=[],
-        headers={'Content-Type': 'application/json'}
-    ))
     data = json.dumps({
         'token': token,
         'channel_id': channel_id,
-        'u_id': u_id
+        'u_id': u_id 
     }).encode('utf-8')
+
     req = urllib.request.Request(
         f"{BASE_URL}/channel/addowner",
         data=data,
@@ -136,16 +107,12 @@ def test_channel_addowner():
 
 def test_channel_removeowner():
 # channel_removeowner (POST)
-    urllib.request.urlopen(urllib.request.Request(
-        f"{BASE_URL}/workspace/reset",
-        data=[],
-        headers={'Content-Type': 'application/json'}
-    ))
     data = json.dumps({
         'token': token,
         'channel_id': channel_id,
         'u_id': u_id
     }).encode('utf-8')
+
     req = urllib.request.Request(
         f"{BASE_URL}/channel/removeowner",
         data=data,
